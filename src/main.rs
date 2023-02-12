8f22dbd6e62fadd31073eebac54250926cee2b60
Auracle <virgilfabre@outlook.com>
2023-02-12 17:26:18 -0500

    Keep alive

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,44 @@
 use dotenv::var;
-
 use std::io::{BufRead, BufReader, Write};
 use std::net::TcpStream;
 
 pub const HOST: &str = "irc.ppy.sh"; // TODO backup server address
 pub const PORT: u16 = 6667;
 
-struct Connection {
-    stream: TcpStream,
-    username: String,
-}
-
 fn main() {
     let user = var("OSU_USERNAME").expect("OSU_USERNAME must be set");
     let pass = var("OSU_IRC_AUTH").expect("OSU_IRC_AUTH must be set");
 
-    let address = format!("{}:{}", HOST, PORT);
-    let mut connection = Connection {
-        stream: TcpStream::connect(address).expect("Failed to connect to server"),
-        username: user.to_string(),
-    };
+    // connect to a TcpStream
+    let stream =
+        TcpStream::connect(format!("{}:{}", HOST, PORT)).expect("Failed to connect to server");
 
+    // create a buffer reader
+    let mut reader = BufReader::new(stream);
+
+    // login
     let login = format!("PASS {}\r\nNICK {}\r\n", pass, user.replace(" ", "_"));
-    connection
-        .stream
-        .write_all(login.as_bytes())
-        .expect("Failed to login");
+    reader.get_mut().write(login.as_bytes()).unwrap();
 
-    let reader = BufReader::new(&connection.stream);
-    for line in reader.lines() {
-        let line = line.unwrap();
-        println!("{}", line);
+    // read lines from the server
+    let mut line = String::new();
+    loop {
+        reader.read_line(&mut line).expect("Failed to read line");
+        if line.is_empty() {
+            continue;
+        }
+
+        if line.contains("QUIT") {
+            continue;
+        }
+
+        if line.starts_with("PING") {
+            reader
+                .get_mut()
+                .write_all(line.replace("PING", "PONG").as_bytes())
+                .expect("Failed to write PONG");
+        }
+        println!("'{}'", line);
+        line.clear();
     }
 }
